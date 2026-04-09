5e59c4e384264ec5625d1ea6ba82d19b3b0ba2f3
Moutsita <moutsitarovincy@gmail.com>
2026-04-09 13:18:05 +0100

    Initialisation du projet

---
+++ insertion_donnees.sql
@@ -0,0 +1,20 @@
+/* -----------------------------------------------------------------------
+------------- INSERTION DES DONNEES DANS LES TABLES DE RAW --------------
+ -----------------------------------------------------------------------*/
+
+/* Remplacer la valeur de la clé primaire auto_incrément
+ par la valeur NULL */
+
+ ----- Table N°1 : Category ------------
+INSERT INTO category (id, intitule, created_at) VALUES
+(NULL, "Romance","2005-12-30"),
+(NULL, "", ""),
+(NULL, "", "");
+----- Table N°2: Books -----
+
+
+----- Table N°3: Customers ------
+
+----- Table N°4:  Facures -------
+
+----- Table N°5 : Ventes ------------
