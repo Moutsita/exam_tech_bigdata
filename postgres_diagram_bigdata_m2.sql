f31743bae83fb834d527e10cccd3f830ca04d1ba
Moutsita <moutsitarovincy@gmail.com>
2026-04-11 20:01:36 +0100

    mise à jour de la BD, des schémas et tables en fonction de Snowflake

--- postgres_diagram_bigdata_m2.sql
+++ postgres_diagram_bigdata_m2.sql
@@ -2,13 +2,13 @@
 ---------------------- Script SQL de BOOKSHOP ----------------------
 ------------------------------------------------------------------*/
 
-// ---------------- CREATION DE LA BASE DONNEE ---------------
+-- CREATION DE LA BASE DONNEE
 CREATE DATABASE IF NOT EXISTS BOOKSHOP;
 
-// ------- Utilisation de la base de donnée -------
-USE BOOKSHOP;
+-- Utilisation de la base de donnée
+USE DATABASE BOOKSHOP;
 
---- Création des schémas ---
+-- Création des schémas
 CREATE SCHEMA IF NOT EXISTS RAW;
 CREATE SCHEMA IF NOT EXISTS STAGGING;
 CREATE SCHEMA IF NOT EXISTS WAREHOUSE;
@@ -17,69 +17,69 @@ CREATE SCHEMA IF NOT EXISTS MARTS;
 /* -----------------------------------------------------------------
 -------------------- Création des tables de RAW --------------------
 ------------------------------------------------------------------*/
-USE RAW;
+USE SCHEMA RAW;
 
 /* ------------ Table N°1: Category ------------------ */
 CREATE TABLE IF NOT EXISTS category (
-    id INTEGER AUTO_INCREMENT,
+    id INTEGER AUTOINCREMENT START 1 INCREMENT 1,
     intitule VARCHAR(100),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP(),
     CONSTRAINT pk_category PRIMARY KEY (id)
 );
 
 /* ------------ Table N°2: Books ------------------ */
 CREATE TABLE IF NOT EXISTS books (
-    id INTEGER AUTO_INCREMENT,
+    id INTEGER AUTOINCREMENT START 1 INCREMENT 1,
     category_id INTEGER,
     code VARCHAR(30),
     intitule VARCHAR(100),
     isbn_10 VARCHAR(30),
     isbn_13 VARCHAR(30),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP(),
 
-    -- Clés primaire et étrangères --
+    -- Clés primaire et étrangères
     CONSTRAINT pk_books PRIMARY KEY(id),
     CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
-/* ------------ Table N°3: costumers ------------------ */
+/* ------------ Table N°3: customers ------------------ */
 CREATE TABLE IF NOT EXISTS customers(
-    id INTEGER AUTO_INCREMENT,
+    id INTEGER AUTOINCREMENT START 1 INCREMENT 1,
     code VARCHAR(30),
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP(),
 
-    -- Clé primaire --
+    -- Clé primaire
     CONSTRAINT pk_customers PRIMARY KEY(id)
 );
 
 /* ------------ Table N°4: Factures ------------------ */
 CREATE TABLE IF NOT EXISTS factures(
-    id INTEGER AUTO_INCREMENT,
+    id INTEGER AUTOINCREMENT START 1 INCREMENT 1,
     code VARCHAR(30),
     date_edit VARCHAR(30),
     customers_id INTEGER,
     qte_totale INTEGER,
     total_amount FLOAT,
     total_paid FLOAT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP(),
 
-    -- Clés primaires et étrangères --
+    -- Clés primaires et étrangères
     CONSTRAINT pk_factures PRIMARY KEY(id),
-    CONSTRAINT fk_costumers FOREIGN KEY (customers_id) REFERENCES customers(id)
+    CONSTRAINT fk_customers FOREIGN KEY (customers_id) REFERENCES customers(id)
 );
 
 /* ------------ Table N°5: Ventes ------------------ */
 CREATE TABLE IF NOT EXISTS ventes(
-    id INTEGER AUTO_INCREMENT,
+    id INTEGER AUTOINCREMENT START 1 INCREMENT 1,
     code VARCHAR(30),
     date_edit VARCHAR(30),
     factures_id INTEGER,
     books_id INTEGER,
     pu FLOAT,
     qte INTEGER,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    created_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP(),
 
     -- Clés primaire et étrangères
     CONSTRAINT pk_ventes PRIMARY KEY(id),
