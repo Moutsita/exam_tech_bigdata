1a48d0b5653d12923f15afbb914adae5513245d9
Moutsita <moutsitarovincy@gmail.com>
2026-04-16 12:06:02 +0100

    Mise à jour des fichiers pour Postgres

--- README.md
+++ README.md
@@ -1,23 +1,28 @@
-# Clonage du dépôt en ligne
+### Clonage du dépôt en ligne
 ''' bash
 git clone https://github.com/Moutsita/exam_tech_bigdata.git
 '''
 
-# Récupération des mises à jour sur la branche principale
+### Récupération des mises à jour sur la branche principale
 ''' bash
 git pull origin main
 '''
 
-# Lancement du programme
+### Lancement du programme
 ''' bash
 docker-compose up -d --build
 '''
-# Arrêt du programme
+### Arrêt du programme
 ''' bash
 docker-compose down
 '''
 
-# Initialistion de dbt et cr&ation des dépôts
+### Accéder au password de Airflow
+''' bash
+docker logs airflow_exam | Select-String "login"
+'''
+
+### Initialistion de dbt et cr&ation des dépôts
 ''' bash
 docker exec -it airflow_exam bash -c "cd /opt/airflow/dbt && dbt init exam_tech_bigdata"
 '''
--- init_db/01_postgres_diagram_bigdata_m2.sql
+++ init_db/01_postgres_diagram_bigdata_m2.sql
@@ -1,13 +1,27 @@
--- Création des tables dans le schéma public de Postgres (Source locale)
-CREATE TABLE category (
+-- Création de la base (à exécuter hors connexion)
+/*  CREATE DATABASE bookshop;  */
+
+-- Ensuite se connecter à la base bookshop
+
+-- Création des schémas
+CREATE SCHEMA IF NOT EXISTS raw;
+CREATE SCHEMA IF NOT EXISTS stagging;
+CREATE SCHEMA IF NOT EXISTS warehouse;
+CREATE SCHEMA IF NOT EXISTS marts;
+
+-- =========================
+-- TABLES RAW
+-- =========================
+
+CREATE TABLE raw.category (
     id SERIAL PRIMARY KEY,
     intitule VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE books (
+CREATE TABLE raw.books (
     id SERIAL PRIMARY KEY,
-    category_id INTEGER REFERENCES category(id),
+    category_id INTEGER REFERENCES raw.category(id),
     code VARCHAR(30),
     intitule VARCHAR(100),
     isbn_10 VARCHAR(30),
@@ -15,7 +29,7 @@ CREATE TABLE books (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE customers (
+CREATE TABLE raw.customers (
     id SERIAL PRIMARY KEY,
     code VARCHAR(30),
     first_name VARCHAR(50),
@@ -23,23 +37,23 @@ CREATE TABLE customers (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE factures (
+CREATE TABLE raw.factures (
     id SERIAL PRIMARY KEY,
     code VARCHAR(30),
-    date_edit VARCHAR(30), -- Format YYYYMMDD [cite: 271]
-    customers_id INTEGER REFERENCES customers(id),
+    date_edit VARCHAR(30),
+    customers_id INTEGER REFERENCES raw.customers(id),
     qte_totale INTEGER,
     total_amount FLOAT,
     total_paid FLOAT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE ventes (
+CREATE TABLE raw.ventes (
     id SERIAL PRIMARY KEY,
     code VARCHAR(30),
     date_edit VARCHAR(30),
-    factures_id INTEGER REFERENCES factures(id),
-    books_id INTEGER REFERENCES books(id),
+    factures_id INTEGER REFERENCES raw.factures(id),
+    books_id INTEGER REFERENCES raw.books(id),
     pu FLOAT,
     qte INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
